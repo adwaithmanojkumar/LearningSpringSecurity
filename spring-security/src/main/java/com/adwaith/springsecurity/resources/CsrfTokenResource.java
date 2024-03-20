b1b28d193085c0cee3b2ff1e5826c7ffeb71e8d7
adwaith <adwaithmanojkumar@gmail.com>
2024-03-21 05:18:18 +0530

    Initial commit

---
+++ spring-security/src/main/java/com/adwaith/springsecurity/resources/CsrfTokenResource.java
@@ -0,0 +1,17 @@
+package com.adwaith.springsecurity.resources;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+public class CsrfTokenResource {
+	
+	@GetMapping("/csrf_token")
+	public CsrfToken retrieveCsrfToken(HttpServletRequest request) {
+		return (CsrfToken) request.getAttribute("_csrf");
+	}
+	
+}
