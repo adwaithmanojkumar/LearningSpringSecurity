53b7a08c911dff9eb49aa71301a224d752208f41
adwaith <adwaithmanojkumar@gmail.com>
2024-03-23 12:24:53 +0530

    Initial commit.

---
+++ learnspringsecurity/src/test/java/com/adwaith/learnspringsecurity/LearnspringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adwaith.learnspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnspringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
